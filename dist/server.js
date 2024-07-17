cee1248068f5e79ca6edb0813b852627e163041d
Eliot Tritschler <eliot.tritschler@gmail.com>
2024-07-17 10:28:01 +0200

    Added route for server config

--- dist/server.js
+++ dist/server.js
@@ -7,24 +7,37 @@ exports.startServer = startServer;
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const fs_1 = __importDefault(require("fs"));
 function startServer() {
     const server = (0, express_1.default)();
-    const port = 3000;
-    var counter = 0;
+    const configPath = 'serverConfig.json';
+    var port = 3000, counter = 0;
+    try {
+        const configData = fs_1.default.readFileSync(configPath, 'utf8');
+        const config = JSON.parse(configData);
+        port = config.port;
+        counter = config.counter;
+    }
+    catch (error) {
+        console.log("Error reading config file :", error);
+    }
     server.get('/', (req, res) => {
         res.send('Hello, Node.js!');
     });
-    server.use(express_1.default.static(path_1.default.join(__dirname, '..', 'static_pages', 'death_counter')));
+    server.use(express_1.default.static(path_1.default.join(__dirname, '..', 'static_pages')));
     server.use(body_parser_1.default.json());
     server.get('/death_counter', (req, res) => {
-        res.sendFile(path_1.default.join(__dirname, '..', 'static_pages', 'death_counter', 'index.html'));
+        res.status(200).sendFile(path_1.default.join(__dirname, '..', 'static_pages', 'death_counter', 'index.html'));
     });
     server.post('/death_counter', (req, res) => {
         const { increment } = req.body;
         if (increment == 1) {
             counter++;
         }
-        res.send({ counter: counter });
+        res.status(200).send({ counter: counter });
+    });
+    server.get('/config', (req, res) => {
+        res.status(200).sendFile(path_1.default.join(__dirname, '..', 'static_pages', 'config', 'index.html'));
     });
     server.listen(port, () => {
         console.log(`Server running at http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,31 @@
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
+import fs from "fs";
 
 export function startServer() {
     const server = express();
-    const port: number = 3000;
-    var counter: number = 0;
+    const configPath = 'serverConfig.json';
+    var port: number = 3000, counter: number = 0;
+    try {
+        const configData = fs.readFileSync(configPath, 'utf8');
+        const config = JSON.parse(configData);
+        port = config.port;
+        counter = config.counter;
+    } catch (error) {
+        console.log("Error reading config file :", error);
+    }
     
     server.get('/', (req, res) => {
         res.send('Hello, Node.js!');
     });
     
-    server.use(express.static(path.join(__dirname, '..', 'static_pages', 'death_counter')));
+    server.use(express.static(path.join(__dirname, '..', 'static_pages')));
     
     server.use(bodyParser.json());
     
     server.get('/death_counter', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'static_pages','death_counter', 'index.html'));
+        res.status(200).sendFile(path.join(__dirname, '..', 'static_pages','death_counter', 'index.html'));
     });
     
     server.post('/death_counter', (req, res) => {
@@ -24,7 +33,11 @@ export function startServer() {
         if(increment == 1) {
             counter++;
         }
-        res.send({counter: counter});
+        res.status(200).send({counter: counter});
+    })
+
+    server.get('/config', (req, res) => {
+        res.status(200).sendFile(path.join(__dirname, '..', 'static_pages', 'config', 'index.html'));
     })
     
     server.listen(port, () => {
